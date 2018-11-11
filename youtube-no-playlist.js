15bceb5db6f301a6674402055280eaccc20cb05e
Peter Szabados <kangirigungi@gmail.com>
2018-11-11 19:33:06 +0100

    More youtube workaround

--- youtube-no-playlist.js
+++ youtube-no-playlist.js
@@ -1,33 +1,38 @@
 // ==UserScript==
 // @name         Youtube No Playlist
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Add a link to YouTube playlists to play video without list.
 // @author       You
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
 
-var previousUrl = ""
-
 setInterval(function() {
-    if (document.URL != previousUrl) {
-        let contents = document.querySelector("div#contents");
-        if (contents == null) {
-            return;
-        }
-        previousUrl = document.URL;
+    let allContents = document.querySelectorAll("div#contents");
+    for (let j = 0; j < allContents.length; ++j) {
+        let contents = allContents[j];
         let elements = contents.querySelectorAll("a.ytd-playlist-video-renderer");
+        if (elements.length == 0) {
+            continue;
+        }
         for (let i = 0; i < elements.length; ++i) {
             let match = elements[i].href.match(".*?v=\\w*");
             if (match.length == 0) {
                 continue;
             }
-            let link = document.createElement("a");
-            link.setAttribute("href", match[0]);
-            link.innerHTML = "watch without list";
-            let title = elements[i].querySelector("span#video-title");
-            title.appendChild(link);
+            let link = elements[i].querySelector("a#watch-without-list");
+            if (link != null) {
+                link.setAttribute("href", match[0]);
+            } else {
+                link = document.createElement("a");
+                link.setAttribute("href", match[0]);
+                link.setAttribute("id", "watch-without-list");
+                link.innerHTML = "watch without list";
+                let title = elements[i].querySelector("span#video-title");
+                title.appendChild(link);
+            }
         }
+        break;
     }
 }, 1000);
